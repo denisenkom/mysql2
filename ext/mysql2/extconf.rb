76502c9bf4479061b0e58c7c49bdf369b938e585
Mikhail Denisenko <denisenk@amazon.com>
2017-07-21 15:27:49 -0400

    intermediate

--- ext/mysql2/extconf.rb
+++ ext/mysql2/extconf.rb
@@ -36,8 +36,6 @@ have_func('rb_hash_dup')
 have_func('rb_intern3')
 have_func('rb_big_cmp')
 
-have_const('MYSQL_ENABLE_CLEARTEXT_PLUGIN')
-
 # borrowed from mysqlplus
 # http://github.com/oldmoe/mysqlplus/blob/master/ext/extconf.rb
 dirs = ENV.fetch('PATH').split(File::PATH_SEPARATOR) + %w(
@@ -116,6 +114,7 @@ mysql_h = [prefix, 'mysql.h'].compact.join('/')
 add_ssl_defines(mysql_h)
 have_struct_member('MYSQL', 'net.vio', mysql_h)
 have_struct_member('MYSQL', 'net.pvio', mysql_h)
+have_const('MYSQL_ENABLE_CLEARTEXT_PLUGIN', mysql_h)
 
 # This is our wishlist. We use whichever flags work on the host.
 # -Wall and -Wextra are included by default.
